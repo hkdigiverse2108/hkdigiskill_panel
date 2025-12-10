18a3f5a2e0c1cd7f38ccbb3f8e438cea47f1e86e
hk-kenil <kenilhkdigiverc@gmail.com>
2025-12-10 10:00:10 +0530

    dashboard updates.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2161730282"
+    serviceWorkerVersion: "4085377059"
   }
 });
